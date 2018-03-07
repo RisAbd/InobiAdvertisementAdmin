2ff9b7fc12d9c4508f29193e6b8924cfe20de28a
Sergei <irreversal@rambler.ru>
2018-03-07 11:48:04 +0600

    chore(client)

--- src/js/client.js
+++ src/js/client.js
@@ -14,25 +14,25 @@ import React from 'react';
 import Ripple from './ripple/Ripple';
 
 import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
-// import { HashRouter as Router, Route, Switch } from "react-router-dom";
-// import { Redirect } from 'react-router';
+import { Provider } from 'react-redux';
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { Redirect } from 'react-router';
 import createHistory from 'history/createHashHistory';
 
-// import store from './store.js';
+import store from './store.js';
 
-// import NavBar from './components/Nav';
-// import Footer from './components/Footer';
+import NavBar from './components/Nav';
+import Footer from './components/Footer';
 
-// import LoginChecker from './components/LoginCheckerLayout';
-// import Map from './pages/Map';
-// import Ads from './pages/Ads';
-// import Views from './pages/Views';
-// import BoxUpdates from './pages/BoxUpdates';
-// import NoMatch from './pages/NoMatch';
-// import Stats from './pages/Stats';
+import LoginChecker from './components/LoginCheckerLayout';
+import Map from './pages/Map';
+import Ads from './pages/Ads';
+import Views from './pages/Views';
+import BoxUpdates from './pages/BoxUpdates';
+import NoMatch from './pages/NoMatch';
+import Stats from './pages/Stats';
 
-// import Test from './pages/Test';
+import Test from './pages/Test';
 
 const tag = '@client.js:';
 
@@ -42,31 +42,28 @@ const history = createHistory();
 
 
 ReactDOM.render(
-  // <Provider store={store}>
-  //   <Router history={history}>
-  //     <div class='ia-react-root'>
-  //       <NavBar />
-  //       <Switch>
-  //         <Route path='/login' component={ LoginChecker } />
-  //         <Route path='/ads' component={ Ads } />
-  //         <Route path='/map' component={ Map } />
-  //         <Route path='/views' component={ Views } />
-  //         <Route path='/box_updates' component={ BoxUpdates } />
-  //         <Route path='/test' component={ Test } />
-  //         <Route path='/stats' component={ Stats } />
+  <Provider store={store}>
+    <Router history={history}>
+      <div class='ia-react-root'>
+        <NavBar />
+        <Switch>
+          <Route path='/login' component={ LoginChecker } />
+          <Route path='/ads' component={ Ads } />
+          <Route path='/map' component={ Map } />
+          <Route path='/views' component={ Views } />
+          <Route path='/box_updates' component={ BoxUpdates } />
+          <Route path='/test' component={ Test } />
+          <Route path='/stats' component={ Stats } />
 
-  //         <Route exact path='/' render={ 
-  //           () => <Redirect from='/' to={
-  //             store.getState().login.loggedIn ? '/stats' : '/login'
-  //           } /> 
-  //         } />
+          <Route exact path='/' render={ 
+            () => <Redirect from='/' to={
+              store.getState().login.loggedIn ? '/stats' : '/login'
+            } /> 
+          } />
 
-  //         <Route component={ NoMatch } />
-  //       </Switch>
-  //     </div>
-  //   </Router>
-  // </Provider>,
-  <Ripple>
-    <button onClick={ () => print("clicked") } style={{width: 100, height: 50, backgroundColor: 'red'}} />
-  </Ripple>,
+          <Route component={ NoMatch } />
+        </Switch>
+      </div>
+    </Router>
+  </Provider>,
    app);
