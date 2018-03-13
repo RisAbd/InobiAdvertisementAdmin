308d578559a11f17be59fedb93cccbdb304e19bc
Sergei <irreversal@rambler.ru>
2018-03-13 12:48:27 +0600

    add(components/AdStats/Generals): translation

--- src/js/components/AdStats/Generals.js
+++ src/js/components/AdStats/Generals.js
@@ -14,43 +14,44 @@ const tag = '@AdStats.Generals:'
 export default class Generals extends React.Component {
   render() {
 
-    const { request, redirect } = this.props;
+    const { request, redirect, translate: translateFn} = this.props;
     const { found: ads, interval } = request;
+    const translate = str => translateFn(`Stats.Generals.${str}`);
 
     const namesRow = [
-      'Name', 
+      translate('name'),
       ads.length === 0 ? '-' : ads.map((ad, i) => adDescriptor(ad, 'title', 'id', 20)).join(' + '),
     ];
     const intervalRow = [
-      'Duration',
+      translate('interval'),
       Object.keys(interval).length === 0 ? '-' : `${unix(interval.start).format('YYYY.MM.DD')}-${unix(interval.end).format('YYYY.MM.DD')}`,
     ];
     // const createdRow = [
-    //   'Created', 
+    //   'Created',
     //   ads.length === 0 ? '-' : unix(Math.min(...ads.map((ad, i) => ad.created))).format('DD.MM.YYYY HH:mm:ss'),
     // ];
     const DESCRIPTION_MAX_LENGTH = 45;
     const description = ads.filter(ad => (typeof ad.description === 'string')).map((ad, i) => `${ad.description}`).join(', ') || [''];
     const descriptionRow = [
-      'Description', 
+      translate('description'),
       ads.length === 0 ? '-' : (description.length > DESCRIPTION_MAX_LENGTH) ? (description.substr(0, DESCRIPTION_MAX_LENGTH) + '...') : description,
     ];
     const redirectRow = [
-      'Redirect', 
+      translate('redirect'),
       Object.keys(redirect).length === 0 ? '-' : `about ${mround(redirect.ratio, 0)}%`,
     ];
     const REDIRECT_TO_MAX_LENGTH = 35;
     const redirectToString = set(ads.map((ad, i) => ad.redirect_url)).join(', ');
     const redirectToRow = [
-      'Redirect to', 
+      translate('redirect-to'),
       ads.length === 0 ? '-' : redirectToString.length > REDIRECT_TO_MAX_LENGTH ? redirectToString.slice(0, REDIRECT_TO_MAX_LENGTH)+'...' : redirectToString,
     ];
     const mediaTypesRow = [
-      'Media Type', 
+      translate('media-type'),
       ads.length === 0 ? '-' : set(ads.map((ad, i) => ad.type)).join(', '),
     ];
     const durationsRow = [
-      'Guarantied duration per view', 
+      translate('guarantied-duration'),
       ads.length === 0 ? '-' : `${Math.round(Math.min(...ads.map((ad, i) => ad.duration))*10)/10} seconds`,
     ];
 
@@ -66,7 +67,7 @@ export default class Generals extends React.Component {
     ];
     const spans= [1, 1];
 
-    return <AdInfo title='General Information' >
+    return <AdInfo title={translate('heading')} >
       <Table rows={ rows } spans={ spans } />
     </AdInfo>;
   }
@@ -79,6 +80,7 @@ Generals.propTypes = {
     interval: PropTypes.object.isRequired,
   }).isRequired,
   redirect: PropTypes.object.isRequired,
+  translate: PropTypes.func.isRequired,
 };
 
 Generals.defaultProps = {
@@ -86,5 +88,5 @@ Generals.defaultProps = {
     found: [],
     interval: {},
   },
-  redirect: {},
+  redirect: {}
 };
