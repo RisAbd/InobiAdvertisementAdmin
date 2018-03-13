e2e17e5af107860eec7b6c0ef56e7a4df0a44cbb
Sergei <irreversal@rambler.ru>
2018-03-13 12:48:36 +0600

    add(components/AdStats/ViewReport): translation

--- src/js/components/AdStats/ViewReport.js
+++ src/js/components/AdStats/ViewReport.js
@@ -12,7 +12,8 @@ const tag = '@AdStats.ViewReport:'
 export default class ViewReport extends React.Component {
   render() {
 
-    let { views } = this.props;
+    const { views, translate: translateFn } = this.props;
+    const translate = str => translateFn(`Stats.Views.${str}`);
 
     let [ _, total ] = views.time_average;
     let { uniqueness: uniques } = views;
@@ -20,15 +21,15 @@ export default class ViewReport extends React.Component {
     const { views_per_day: vpd, views_per_week: vpw } = total;
     const { uniques_per_day: upd, uniques_per_week: upw } = uniques
 
-    const headers = ['', 'Views', 'Unique views'];
+    const headers = ['', translate('views'), translate('unique-views')];
     const rows = [
-      ['Average per 1 day', mround(vpd, 1) || '-', mround(upd, 1) || '-'],
-      ['Average per 1 week', mround(vpw, 1) || '-', mround(upw, 1) || '-'],
-      ['Total for interval', uniques.total || '-', uniques.uniques || '-'],
+      [translate('avg-per-day'), mround(vpd, 1) || '-', mround(upd, 1) || '-'],
+      [translate('avg-per-week'), mround(vpw, 1) || '-', mround(upw, 1) || '-'],
+      [translate('total'), uniques.total || '-', uniques.uniques || '-'],
     ];
     const spans= [2, 1, 1];
 
-    return <AdInfo title='View report'>
+    return <AdInfo title={translate('heading')}>
       <Table headers={ headers } rows={ rows } spans={ spans } />
     </AdInfo>;
   }
@@ -39,12 +40,13 @@ ViewReport.propTypes = {
   views: PropTypes.shape({
     time_average: PropTypes.arrayOf(PropTypes.object).isRequired,
     uniqueness: PropTypes.object,
-  })
+  }),
+  translate: PropTypes.func.isRequired,
 };
 
 ViewReport.defaultProps = {
   views: {
-    time_average: [{}, {}], 
+    time_average: [{}, {}],
     uniqueness: {}
   }
 }
