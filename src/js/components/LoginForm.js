c52337e1f62d2fef528c8a3d82adc7cf8f33d2ef
Sergei <irreversal@rambler.ru>
2018-03-14 16:52:16 +0600

    add(components/LoginForm): translations, styles update

--- src/js/components/LoginForm.js
+++ src/js/components/LoginForm.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { getTranslate } from 'react-localize-redux';
 
 import { login } from '../actions/loginActions';
 
+const mapStateToProps = ({ login, locale }) => ({
+  login,
+  translate: getTranslate(locale),
+});
 
-@connect((store) => {
-  return {
-    login: { ...store.login },
-  }
-})
+@connect(mapStateToProps)
 export default class LoginForm extends React.Component {
 
   login = (event) => {
@@ -17,18 +18,18 @@ export default class LoginForm extends React.Component {
   };
 
   render() {
-    const { disabled } = this.props;
+    const { disabled, translate } = this.props;
     return <form onSubmit={ this.login } class='ia-login-form ia-centered'>
       <input
         class='ia-login-form__text-input'
-        placeholder='Admin key'
+        placeholder={ translate('password') }
         type='password'
         name='admin_key'
         disabled={ disabled } />
       <input
         class='ia-login-form__submit-input'
         type='submit'
-        value='Login'
+        value={ translate('login') }
         disabled={ disabled } />
     </form>;
   }
