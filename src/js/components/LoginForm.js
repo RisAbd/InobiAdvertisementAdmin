3ee6de0026cc8750df16f1ec2ad479cb5fd712e2
Sergei <irreversal@rambler.ru>
2018-03-15 19:09:00 +0600

    update(login): add login field

--- src/js/components/LoginForm.js
+++ src/js/components/LoginForm.js
@@ -9,12 +9,26 @@ const mapStateToProps = ({ login, locale }) => ({
   translate: getTranslate(locale),
 });
 
+const isEmail = str => /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(str);
+
 @connect(mapStateToProps)
 export default class LoginForm extends React.Component {
 
-  login = (event) => {
-    this.props.dispatch(login(event.target.admin_key.value));
-    event.preventDefault();
+  login = (e) => {
+    e.preventDefault();
+    const { target } = e;
+    const { login: loginCredentialEl, password: passwordEl } = target;
+    const loginValue = loginCredentialEl.value;
+    const passwordValue = passwordEl.value ? passwordEl.value.trim() : '';
+
+    const credentialKey = isEmail(loginValue) ? 'email' : 'username';
+
+    const credentials = {
+      [credentialKey]: loginValue,
+      pwd: passwordValue,
+      login: loginValue,
+    };
+    this.props.dispatch(login(credentials));
   };
 
   render() {
@@ -22,9 +36,15 @@ export default class LoginForm extends React.Component {
     return <form onSubmit={ this.login } class='ia-login-form ia-centered'>
       <input
         class='ia-login-form__text-input'
+        placeholder={ translate('login-or-email') }
+        type='text'
+        name='login'
+        disabled={ disabled } />
+      <input
+        class='ia-login-form__text-input'
         placeholder={ translate('password') }
         type='password'
-        name='admin_key'
+        name='password'
         disabled={ disabled } />
       <input
         class='ia-login-form__submit-input'
