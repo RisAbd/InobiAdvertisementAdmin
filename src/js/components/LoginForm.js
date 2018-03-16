72aef11f811b7a42223eec4755cd70037e4ecc6a
Sergei <irreversal@rambler.ru>
2018-03-16 12:50:38 +0600

    add(components/LoginForm): language select

--- src/js/components/LoginForm.js
+++ src/js/components/LoginForm.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { getTranslate } from 'react-localize-redux';
+import { getTranslate, getActiveLanguage, setActiveLanguage } from 'react-localize-redux';
+
+import { LANGUAGES, LANGUAGE_LS_KEY } from "../constants";
 
 import { login } from '../actions/loginActions';
+
+import { Select } from "./Select";
+
+const isEmail = str => /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(str);
+
+const SELECTOR_LANGUAGES = LANGUAGES.map(({ code, name }) => ({
+  label: name,
+  value: code,
+}));
 
 const mapStateToProps = ({ login, locale }) => ({
   login,
   translate: getTranslate(locale),
+  activeLanguage: getActiveLanguage(locale).code,
 });
-
-const isEmail = str => /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(str);
 
 @connect(mapStateToProps)
 export default class LoginForm extends React.Component {
+  handleLanguageChange = ({ target }) => {
+    const { value } = target;
+
+    this.props.dispatch(setActiveLanguage(value));
+    localStorage.setItem(LANGUAGE_LS_KEY, value);
+  };
 
   login = (e) => {
     e.preventDefault();
@@ -32,25 +48,34 @@ export default class LoginForm extends React.Component {
   };
 
   render() {
-    const { disabled, translate } = this.props;
-    return <form onSubmit={ this.login } class='ia-login-form ia-centered'>
-      <input
-        class='ia-login-form__text-input'
-        placeholder={ translate('login-or-email') }
-        type='text'
-        name='login'
-        disabled={ disabled } />
-      <input
-        class='ia-login-form__text-input'
-        placeholder={ translate('password') }
-        type='password'
-        name='password'
-        disabled={ disabled } />
-      <input
-        class='ia-login-form__submit-input'
-        type='submit'
-        value={ translate('login') }
-        disabled={ disabled } />
-    </form>;
+    const { disabled, translate, activeLanguage } = this.props;
+
+    return (
+      <form onSubmit={ this.login } class='ia-login-form ia-centered'>
+        <Select
+          style={{ margin: '0 60px'}}
+          items={SELECTOR_LANGUAGES}
+          currentValue={activeLanguage}
+          onChange={this.handleLanguageChange}
+          />
+        <input
+          class='ia-login-form__text-input'
+          placeholder={ translate('login-or-email') }
+          type='text'
+          name='login'
+          disabled={ disabled } />
+        <input
+          class='ia-login-form__text-input'
+          placeholder={ translate('password') }
+          type='password'
+          name='password'
+          disabled={ disabled } />
+        <input
+          class='ia-login-form__submit-input'
+          type='submit'
+          value={ translate('login') }
+          disabled={ disabled } />
+      </form>
+    );
   }
 }
